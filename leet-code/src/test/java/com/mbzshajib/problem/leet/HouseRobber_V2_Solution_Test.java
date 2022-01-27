4d2f4649242d9c961936d56d2a74e539e138cfb5
Zaman Shajib <mbzshajib@gmail.com>
2022-01-27 18:10:53 +0600

    solved v2 house-robber https://leetcode.com/problems/house-robber

---
+++ leet-code/src/test/java/com/mbzshajib/problem/leet/HouseRobber_V2_Solution_Test.java
@@ -0,0 +1,21 @@
+package com.mbzshajib.problem.leet;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.Suite;
+
+/**
+ * @author: Zaman Shajib
+ * @email: mbshajib@gmail.com
+ * @git: https://github.com/mbzshajib
+ * @linkedin: https://www.linkedin.com/in/mbzshajib/
+ * Created on 1/27/2022 at 5:27 PM.
+ */
+@Suite
+public class HouseRobber_V2_Solution_Test {
+    @Test
+    void testAll() {
+        Assertions.assertEquals(4, new HouseRobber_V2_Solution().rob(new int[]{1, 2, 3, 1}));
+        Assertions.assertEquals(12, new HouseRobber_V2_Solution().rob(new int[]{2,7,9,3,1}));
+    }
+}
