b05c1141fe12951db4ecf1948dc1be52c678ac7b
Zaman Shajib <mbzshajib@gmail.com>
2022-01-27 16:42:02 +0600

    solved v2 climbing-stairs https://leetcode.com/problems/climbing-stairs

---
+++ leet-code/src/test/java/com/mbzshajib/problem/leet/ClimbingStairs_BottomUp_Solution_Test.java
@@ -0,0 +1,24 @@
+package com.mbzshajib.problem.leet;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.Suite;
+
+/**
+ * @author: Zaman Shajib
+ * @email: mbshajib@gmail.com
+ * @git: https://github.com/mbzshajib
+ * @linkedin: https://www.linkedin.com/in/mbzshajib/
+ * Created on 1/27/2022 at 4:22 PM.
+ */
+@Suite
+public class ClimbingStairs_BottomUp_Solution_Test {
+    @Test
+    void testAll() {
+        Assertions.assertEquals(1, new ClimbingStairs_BottomUp_Solution().climbStairs(1));
+        Assertions.assertEquals(2,new ClimbingStairs_BottomUp_Solution().climbStairs(2));
+        Assertions.assertEquals(3,new ClimbingStairs_BottomUp_Solution().climbStairs(3));
+        Assertions.assertEquals(1836311903,new ClimbingStairs_BottomUp_Solution().climbStairs(45));
+
+    }
+}
