45840d0929b81f0b8a844b588b4101511461934c
Zaman Shajib <mbzshajib@gmail.com>
2022-01-29 01:35:42 +0600

    solved length-of-last-word https://leetcode.com/problems/length-of-last-word

---
+++ leet-code/src/test/java/com/mbzshajib/problem/leet/LengthOfLastWord_Solution_Test.java
@@ -0,0 +1,23 @@
+package com.mbzshajib.problem.leet;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.Suite;
+
+/**
+ * @author: Zaman Shajib
+ * @email: mbshajib@gmail.com
+ * @git: https://github.com/mbzshajib
+ * @linkedin: https://www.linkedin.com/in/mbzshajib/
+ * Created on 1/29/2022 at 1:26 AM.
+ */
+@Suite
+public class LengthOfLastWord_Solution_Test {
+    @Test
+    void testAll() {
+        Assertions.assertEquals(5, new LengthOfLastWord_Solution().lengthOfLastWord("Hello World"));
+        Assertions.assertEquals(4, new LengthOfLastWord_Solution().lengthOfLastWord("   fly me   to   the moon  "));
+        Assertions.assertEquals(3, new LengthOfLastWord_Solution().lengthOfLastWord("   fly me   to   the   "));
+        Assertions.assertEquals(6, new LengthOfLastWord_Solution().lengthOfLastWord("luffy is still joyboy"));
+    }
+}
