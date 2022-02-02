00e0578fa63dbab93c5bb3305e92e9b1f45c7a10
Zaman Shajib <mbzshajib@gmail.com>
2022-02-03 00:31:15 +0600

    solved best-time-to-buy-and-sell-stock https://leetcode.com/problems/best-time-to-buy-and-sell-stock

--- leet-code/src/main/java/com/mbzshajib/problem/leet/BestTimeToBuyAndSellStock_Solution.java
+++ leet-code/src/main/java/com/mbzshajib/problem/leet/BestTimeToBuyAndSellStock_Solution.java
@@ -11,44 +11,21 @@ import java.util.Stack;
  * Created on 12/6/21 at 9:16 PM.
  */
 public class BestTimeToBuyAndSellStock_Solution {
-    public static void main(String[] args) {
-        System.out.println(maxProfit(new int[]{7,6,4,3,1}));
-    }
-
-
     public static int maxProfit(int[] prices) {
-        if (prices == null || prices.length <= 2) return 0;
-        int profit = 0;
-        int counter = 1;
-        for (int i = 0; i < prices.length; i++) {
-            for (int j = i + 1; j < prices.length; j++) {
-                int diff = prices[j] - prices[i];
-                if (diff > profit) profit = diff;
-            }
-        }        System.out.println(Arrays.toString(prices));
-
-        return profit;
-
-    }
-
-    public static int maxProfitOld(int[] prices) {
-        if (prices == null || prices.length <= 2) return 0;
-        System.out.println(Arrays.toString(prices));
-        int profit = 0;
-        Stack<Integer> max = new Stack<>();
-        Stack<Integer> min = new Stack<>();
-        max.push(prices[0]);
-        min.push(prices[0]);
-        for (int i = 1; i<prices.length;i++){
-            if(max.peek()<prices[i]){
-                max.push(prices[i]);
-            }
-            if(min.peek()>prices[i]){
-                min.push(prices[i]);
-            }
+        if (prices.length == 1) return 0;
+        if (prices.length == 2) {
+            if (prices[0] > prices[1]) return 0;
+            else return prices[1] - prices[0];
         }
-
-        return profit;
+        int result = 0;
+        int[] calculated = new int[prices.length];
+        calculated[0] = 0;
+        result = Math.max(result, prices[1] - prices[0]);
+        for (int i = 1; i < calculated.length; i++) {
+            calculated[i] = Math.max(prices[i] - prices[i - 1] + calculated[i - 1], 0);
+            result = Math.max(result, calculated[i]);
+        }
+        return result;
 
     }
 }
---
+++ leet-code/src/test/java/com/mbzshajib/problem/leet/BestTimeToBuyAndSellStock_Solution_Test.java
@@ -0,0 +1,22 @@
+package com.mbzshajib.problem.leet;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.Suite;
+
+/**
+ * @author: Zaman Shajib
+ * @email: mbshajib@gmail.com
+ * @git: https://github.com/mbzshajib
+ * @linkedin: https://www.linkedin.com/in/mbzshajib/
+ * Created on 2/3/2022 at 12:12 AM.
+ */
+@Suite
+public class BestTimeToBuyAndSellStock_Solution_Test {
+    @Test
+    void testAll() {
+        Assertions.assertEquals(5, new BestTimeToBuyAndSellStock_Solution().maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        Assertions.assertEquals(0, new BestTimeToBuyAndSellStock_Solution().maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+
+}
